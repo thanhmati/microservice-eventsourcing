7725e9acca5972b2876df1c7a43aea16d47f116b
thanhmati <thanh.do@mati.com.vn>
2024-06-22 22:17:55 +0700

    init discovery server

---
+++ discoverserver/src/test/java/com/ltfullstack/discoverserver/DiscoverserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltfullstack.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
