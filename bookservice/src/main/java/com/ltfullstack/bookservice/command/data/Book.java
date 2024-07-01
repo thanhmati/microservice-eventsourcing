4c2bf2ba4ead2e9d0cf163b4c0d033fd5afe1af1
thanhmati <thanh.do@mati.com.vn>
2024-07-02 00:22:44 +0700

    create books table

---
+++ bookservice/src/main/java/com/ltfullstack/bookservice/command/data/Book.java
@@ -0,0 +1,24 @@
+package com.ltfullstack.bookservice.command.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String author;
+    private Boolean isReady;
+
+}
