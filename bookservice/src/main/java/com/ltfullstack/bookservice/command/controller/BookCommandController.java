69cf1ea2abe60fc555b9c36464fd9f26e9959e9f
thanhmati <thanh.do@mati.com.vn>
2024-07-14 00:29:34 +0700

    Add validation request body

--- bookservice/src/main/java/com/ltfullstack/bookservice/command/controller/BookCommandController.java
+++ bookservice/src/main/java/com/ltfullstack/bookservice/command/controller/BookCommandController.java
@@ -4,6 +4,7 @@ import com.ltfullstack.bookservice.command.command.CreateBookCommand;
 import com.ltfullstack.bookservice.command.command.DeleteBookCommand;
 import com.ltfullstack.bookservice.command.command.UpdateBookCommand;
 import com.ltfullstack.bookservice.command.model.BookRequestModel;
+import jakarta.validation.Valid;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class BookCommandController {
     private CommandGateway commandGateway;
 
     @PostMapping
-    public String addBook(@RequestBody BookRequestModel model){
+    public String addBook(@Valid @RequestBody BookRequestModel model){
         CreateBookCommand command = new CreateBookCommand(UUID.randomUUID().toString(),model.getName(),model.getAuthor(),true);
         return commandGateway.sendAndWait(command);
     }
--- bookservice/src/main/java/com/ltfullstack/bookservice/command/model/BookRequestModel.java
+++ bookservice/src/main/java/com/ltfullstack/bookservice/command/model/BookRequestModel.java
@@ -1,5 +1,7 @@
 package com.ltfullstack.bookservice.command.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,11 @@ import lombok.Setter;
 public class BookRequestModel {
     private String id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
     private String name;
+    @NotBlank(message = "Author is mandatory")
     private String author;
+
     private Boolean isReady;
 }
