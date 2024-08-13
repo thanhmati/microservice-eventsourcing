f716105a4faf9c68282b46af7406370618a6297b
thanhmati <thanh.do@mati.com.vn>
2024-08-13 23:54:38 +0700

    init notification service

---
+++ notificationservice/src/main/java/com/ltfullstacak/notificationservice/NotificationserviceApplication.java
@@ -0,0 +1,14 @@
+package com.ltfullstacak.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.ltfullstack.notificationservice","com.ltfullstack.commonservice"})
+public class NotificationserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationserviceApplication.class, args);
+	}
+
+}
