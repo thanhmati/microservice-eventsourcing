27540d79d43b774e4a9fc55f30c87d7cb908472d
thanhmati <thanh.do@mati.com.vn>
2024-09-01 01:14:29 +0700

    Send email with html template

--- commonservice/src/main/java/com/ltfullstack/commonservice/services/EmailService.java
+++ commonservice/src/main/java/com/ltfullstack/commonservice/services/EmailService.java
@@ -1,5 +1,8 @@
 package com.ltfullstack.commonservice.services;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -8,8 +11,14 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -17,6 +26,8 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private Configuration config;
 
     /**
      * Sends an email with optional HTML content and attachment.
@@ -50,4 +61,43 @@ public class EmailService {
             // Handle the exception (retry logic, save to dlq...)
         }
     }
+
+    /**
+     * Sends an email with optional HTML content and attachment.
+     *
+     * @param to         The recipient's email address.
+     * @param subject    The subject of the email.
+     * @param templateName The name of the HTML template file.
+     * @param placeholders A map of placeholders and their replacements.
+     * @param attachment An optional file attachment, can be null.
+     */
+    public void sendEmailWithTemplate(String to, String subject, String templateName, Map<String, Object> placeholders, File attachment) {
+        try {
+
+            Template t = config.getTemplate(templateName);
+            String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, placeholders);
+
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(html, true); // true means the content is HTML
+
+            // Add attachment if provided
+            if (attachment != null) {
+                FileSystemResource fileResource = new FileSystemResource(attachment);
+                helper.addAttachment(fileResource.getFilename(), fileResource);
+            }
+
+            javaMailSender.send(message);
+            log.info("Email sent successfully to {}", to);
+
+        } catch (MessagingException | IOException | TemplateException e) {
+            log.error("Failed to send email to {}", to, e);
+            // Handle the exception (retry logic, save to a dead letter queue, etc.)
+        }
+    }
+
+
 }
