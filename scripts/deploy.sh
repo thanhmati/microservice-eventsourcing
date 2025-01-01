3936b285fd8c4797d021f997fa49622519937ef7
thanhmati <thanh.do@mati.com.vn>
2025-01-01 16:21:01 +0700

    update deploy yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,15 @@ echo "Deploying with Docker username: $DOCKER_USERNAME"
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+echo "Deploying with branch: $BRANCH"
+
+
 cd app/microservice-eventsourcing
 git checkout $BRANCH
 git pull
+
+cat docker-compose.yml
+
 docker compose -f docker-compose.yml down
 docker compose -f docker-compose.yml pull
 docker compose -f docker-compose.yml up -d
