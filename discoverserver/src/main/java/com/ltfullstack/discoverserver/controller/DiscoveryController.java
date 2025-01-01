80418697d519a18173391eaa6240e87391f58351
thanhmati <thanh.do@mati.com.vn>
2025-01-01 15:30:38 +0700

    build pipeline ci cd

---
+++ discoverserver/src/main/java/com/ltfullstack/discoverserver/controller/DiscoveryController.java
@@ -0,0 +1,17 @@
+package com.ltfullstack.discoverserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController()
+@RequestMapping("/api/v1/discovery")
+public class DiscoveryController{
+    @GetMapping()
+    public String welcome() {
+        return "Welcome Lap Trinh Full Stack";
+    }
+    
+}
