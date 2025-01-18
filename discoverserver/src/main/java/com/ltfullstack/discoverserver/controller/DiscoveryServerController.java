2fbb62994432bf54b205f08d1cc98c7e1a76c9cf
thanhmati <thanh.do@mati.com.vn>
2025-01-18 11:52:04 +0700

    update code

---
+++ discoverserver/src/main/java/com/ltfullstack/discoverserver/controller/DiscoveryServerController.java
@@ -0,0 +1,14 @@
+package com.ltfullstack.discoverserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("api/v1/discovery")
+public class DiscoveryServerController {
+    @GetMapping()
+    public String helloWorld(){
+        return "Lap trinh full stack";
+    }
+}
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,6 @@ git checkout $BRANCH
 git pull
 
 docker compose -f docker-compose.yml down
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml pull discoveryserver
+docker compose -f docker-compose.yml up -d discoveryserver
 docker system prune -af
